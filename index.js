ab2c434fd3f7e2ea56961f9af606fe1a84842b51
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-03-29 12:49:30 +0000

    add external module and app variables

---
+++ index.js
@@ -0,0 +1,28 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require('express');
+const path = require('path');
+
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || '8000';
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+
+/**
+ * Server Activation
+ */
