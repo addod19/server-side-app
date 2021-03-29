e93922412b5daa4a15eeaa21a559520c3c050d20
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-03-29 12:53:26 +0000

    add route definition and server activation code

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ const port = process.env.PORT || '8000';
  * Routes Definitions
  */
 
+app.get('/', (req, res) => {
+  res.status(200).send('DanFood: Food for full stack developers');
+})
+
 /**
  * Server Activation
  */
+
+app.listen(port, () => {
+  console.log(`Listening to request on http://localhost:${port}`);
+});
